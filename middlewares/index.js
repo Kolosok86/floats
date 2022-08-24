0e47de329174f549926a3b57c46757db6643bd17
kolosok86 <anohkin.777@mail.ru>
2022-08-24 22:25:26 +0300

    change options.js folder

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,4 @@
-import * as opts from "./options.js";
+import * as opts from "../constants/options.js";
 import convert from 'koa-convert';
 import logger from "koa-logger";
 import ratelimit from "koa-ratelimit";
